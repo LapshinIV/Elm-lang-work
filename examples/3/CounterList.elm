8e075fcfd15b12902b2508a02645dd8fbfe299a3
LapshinIV <ivan.lapshin@39parrot.net>
2016-02-19 12:44:08 +0400

    reduce CounterList

--- examples/3/CounterList.elm
+++ examples/3/CounterList.elm
@@ -5,32 +5,18 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-init ={ counters = [], nextID = 0 }
+init ={ counters = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)] }
 
 
--- UPDATE
-
-type Action = Insert
-
-
-update action model =
-  case action of
-    Insert ->
-      let newCounter = ( model.nextID, Counter.init 0 )
-          newCounters = model.counters ++ [ newCounter ]
-      in
-          { model |
-              counters = newCounters,
-              nextID = model.nextID + 1
-          }
+update model = model
 
 
 view address model =
   let counters = List.map (viewCounter address) model.counters
       remove = button [] [ text "Remove" ]
-      insert = button [ onClick address Insert ] [ text "Add" ]
+      insert = button [] [ text "Add" ]
   in
       div [] ([remove, insert] ++ counters)
 
 
-viewCounter address (model) = Counter.view () model
+viewCounter address (id, model) = Counter.view () model
